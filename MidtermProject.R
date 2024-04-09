c70055da101bc190caf87054da5970012a7d9f88
tsantos06 <159483432+tsantos06@users.noreply.github.com>
2024-04-09 10:35:39 -0400

    Add files via upload

---
+++ MidtermProject.R
@@ -0,0 +1,107 @@
+library(dplyr)
+library(ggplot2)
+hospitals = read.csv("hospitals.csv")
+
+#how big is the dataset
+
+dim(hospitals)
+
+#What are the names of the columns
+
+colnames(hospitals)
+
+colDataTypes= sapply(hospitals, class)
+
+cat(colDataTypes)
+
+#Values missing?
+
+missingvalues = any(is.na(hospitals))
+print(missingvalues)
+#False no values missing
+
+# Hospital num 1064 and 1751 have lowest amount of beds, 3
+
+LowNumBed=hospitals %>% filter(Total.Expense==min(Total.Expense))
+print(LowNumBed)
+
+#1,112 hospitals have babies being delivered
+
+Babies = hospitals %>% filter(Births,or,Not==1)
+count(Babies)
+
+#scatter plot of beds and total expense
+
+ggplot(hospitals, aes(x= beds, y=Total.Expense)) + geom_point()
+
+#admissions and total expense?
+
+ggplot(hospitals, aes(x= Admissions, y=Total.Expense)) + geom_point()
+
+#beds and total expense for hospitals that have babies delivered
+
+ggplot(Babies, aes(x=Beds, y=Total.Expense)) + geom_point()
+
+#Relationship between amount of beds and total expense?
+
+ggplot(hospitals, aes(x=Beds, y=Payroll.Expense)) + geom_point()
+
+#2, descriptive analysis, pie chart
+
+df = data.frame(
+  AdTotExp = c("Admissions", "Total Expense"),
+  Values = c(sum(hospitals$Admissions), sum(hospitals$"Total.Expense"))
+)
+
+df %>% ggplot(aes(x="", y=AdTotExp, fill=Values)) + 
+  geom_bar(stat="identity", width=1) + 
+  coord_polar("y", start=0) +
+  ggtitle("Pie Chart Admissions vs Total Expense")
+
+#Bar Chart
+
+AdCensus = sum(hospitals$Personnel)
+print(AdCensus)
+df2 =data.frame(
+  category = c('Admissions', 'Personnel'),
+  chart = c(sum(hospitals$Admissions), AdCensus)
+)
+ggplot(df2, aes(x=category, y=chart, fill=category)) + geom_bar(stat="identify")
+
+#Line Chart
+
+hospital.line = rank(hospitals$Total.Expense)
+ggplot(hospitals, aes(x=hospital.line)) + geom_line(aes(y=Total.Expense))
+
+#3, Simple Regression
+
+SR = lm(Total.Expense ~ Beds, data=hospitals)
+summary (SR)
+
+#R^2 value = 0.6043
+
+# R-squared value
+
+rsquared <- summary(SR)$r.squared
+print(paste("R-squared value:", rsquared))
+
+# p-values
+pvalues <- summary(SR)$coefficients[, 4]
+print(paste("P-values:", pvalues))
+#p-value = 0.0069
+
+#4, Multivariate Regression
+
+#multivariate linear regression
+MR = lm(Total.Expense ~ Beds + Personnel, data = hospitals)
+
+#summary of regression results
+summary(MR)
+
+#R-squared value
+rsquared <- summary(MR)$r.squared
+print(paste("R-squared value:", rsquared))
+
+#p-values
+pvalues <- summary(MR)$coefficients[, 4]
+print(paste("P-values:", pvalues))
